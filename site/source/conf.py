18748d7ef28ce753ab9afd28077d9ea37171c63c
TOsmanov <timurx.osmanov@intel.com>
2021-12-29 09:54:07 +0300

    update conf.py

--- site/source/conf.py
+++ site/source/conf.py
@@ -66,9 +66,11 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_context = {
-    'css_files': ['../_static/versionig_menu.css', '_static/custom.css', '_static/versionig_menu.css'],
-}
+html_static_path = ['_static', '../_static']
+html_css_files = ['custom.css', 'versionig_menu.css']
+# html_context = {
+#     'css_files': ['../_static/versionig_menu.css', '_static/custom.css', '_static/versionig_menu.css'],
+# }
 # -- Extension configuration -------------------------------------------------
 autodoc_docstring_signature = True
 autodoc_member_order = 'bysource'
@@ -99,5 +101,6 @@ def replace(app, what, name, obj, options, lines):
                     lines[i] = lines[i].replace("|n", "\n").replace("|s", " ")
 
 def setup(app):
+    app.add_css_files('../_static/versionig_menu.css',)
     app.connect('autodoc-skip-member', skip_member)
     app.connect('autodoc-process-docstring', replace)
