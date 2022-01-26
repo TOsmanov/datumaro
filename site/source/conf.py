9045e0869e2c4a80e5c9b56decb2636227c32957
TOsmanov <timurx.osmanov@intel.com>
2022-01-26 12:55:24 +0300

    update conf.py

--- site/source/conf.py
+++ site/source/conf.py
@@ -12,12 +12,14 @@
 
 import os
 import sys
+import shutil
+import glob
+import re
 
 sys.path.insert(0, os.path.abspath('../..'))
 
 import git
 from datumaro.version import VERSION
-from prepair_past_versions import prepair_current_repo
 
 # -- Project information -----------------------------------------------------
 
@@ -37,15 +39,16 @@ extensions = [
     'sphinx.ext.napoleon', # Support for NumPy and Google style docstrings
     'sphinx.ext.autodoc',  # Core library for html generation from docstrings
     'sphinx.ext.viewcode', # Find the source files
-    'myst_parser', # https://www.sphinx-doc.org/en/master/usage/markdown.html
     'sphinxcontrib.mermaid', # Extension allows you to embed Mermaid graphs
     'sphinx_copybutton', # Copy buttons for code blocks
 ]
 
-source_suffix = {
-    '.rst': 'restructuredtext',
-    '.md': 'markdown',
+source_parsers  =  {
+   '.md' :  'recommonmark.parser.CommonMarkParser' ,
 }
+
+source_suffix  =  [ '.rst' ,  '.md' ]
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates', ]
 
@@ -75,7 +78,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static', '_static']
+html_static_path = ['_static', ]
 html_css_files = ['custom.css', ]
 
 # -- Extension configuration -------------------------------------------------
@@ -86,8 +89,60 @@ autodoc_member_order = 'bysource'
 mermaid_params = [
     '--width', '500',
 ]
+# sphinxcontrib-versioning
+# scv_whitelist_branches = ('v0.1.11', 'v0.2.1', 'v0.2.2')
+scv_whitelist_tags = (re.compile(r'^v\d+\.\d+\.\d+$'),)
 
-# Members to be included.
+# Prepair source files for past versions.
+
+# def prepair_files(cwd):
+#     index_md = os.path.join(cwd, 'site', 'source', 'docs', '_index.md')
+#     if os.path.exists(index_md):
+#         os.remove(index_md)
+#     git.checkout('develop', '--', 'site/source/index.rst')
+#     git.checkout('develop', '--', 'site/source/formats/formats.rst')
+#     git.checkout('develop', '--', 'site/source/plugins/plugins.rst')
+#     git.checkout('develop', '--', 'site/source/user-manual/user-manual.rst')
+#     git.checkout('develop', '--', 'site/source/user-manual/command-reference/command-reference.rst')
+
+# def prepair_headers(destination):
+#     files = glob.iglob(os.path.join(destination, '**', '*.md'), recursive=True)
+#     for file in files:
+#         with open(file, 'r+') as f:
+#             lines = f.readlines()
+#         for i, line in enumerate(lines):
+#             if '```mermaid' in line:
+#                 lines[i] = line.replace("```mermaid","```{mermaid}")
+#             if "---" in line:
+#                 lines[i] = ''
+#             if "title: '" in line:
+#                 title = '# ' + line[8:-2] + '\n'
+#                 lines[i] = title
+#             if "description: '" in line:
+#                 description = line[14:-2] + '\n'
+#                 lines[i] = description
+#             exclude_list = [
+#                 "linkTitle: '",
+#                 "weight:",
+#                 ]
+#             for a in exclude_list:
+#                 if a in line:
+#                     lines[i] = ''
+#         with open(file, 'w') as f:
+#             f.writelines(lines)
+
+# def prepair_current_repo():
+#     cwd = os.getcwd()
+#     destination = os.path.join(cwd, 'site', 'source')
+#     prepair_files(cwd, destination)
+#     prepair_headers(destination)
+
+# def source_read_handler(app, docname, source):
+#     if os.path.exists(os.path.join('site', 'source', 'content')):
+#         prepair_current_repo()
+
+# Prepair docstring for autodoc
+
 include_members_list = [
     '__init__',
     '__iter__',
@@ -108,11 +163,7 @@ def replace(app, what, name, obj, options, lines):
                 if not "'|s'" in lines[i]:
                     lines[i] = lines[i].replace("|n", "\n").replace("|s", " ")
 
-def source_read_handler(app, docname, source):
-    if os.path.exists(os.path.join('site', 'source', 'content'):
-        prepair_current_repo()
-
 def setup(app):
     app.connect('autodoc-skip-member', skip_member)
     app.connect('autodoc-process-docstring', replace)
-    app.connect('source-read', source_read_handler)
+    # app.connect('source-read', source_read_handler)
