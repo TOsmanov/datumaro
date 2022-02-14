435f463bce65955f92c27542c3e316a30643896f
TOsmanov <timurx.osmanov@intel.com>
2022-02-14 14:58:49 +0300

    hide '.. code-block' in cli help and fix mistakes

--- datumaro/components/annotation.py
+++ datumaro/components/annotation.py
@@ -109,10 +109,9 @@ class LabelCategories(Categories):
         Creates a LabelCategories from iterable.
 
         Args:
-            iterable: This iterable object can be:
+            - iterable - This iterable object can be:
 
                 - a list of str - will be interpreted as list of Category names
-
                 - a list of positional arguments - will generate Categories
                   with these arguments
 
